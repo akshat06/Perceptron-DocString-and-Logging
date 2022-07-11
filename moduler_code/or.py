90573772e56a9b3486b50c979e682d7ad92545cf
Akshat Pant <akshat.pant06@gmail.com>
2022-07-11 22:31:30 +0530

    logging added for or model

--- moduler_code/or.py
+++ moduler_code/or.py
@@ -1,11 +1,25 @@
+from stat import filemode
 from pip import main
 from utils.all_utils import prepare_data, save_plot
 import pandas as pd
 from utils.model import Perceptron
+import logging,os
+
+gate = "OR gate"
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(
+    filename=os.path.join("logs", "running_logs.log"),
+    level=logging.INFO,
+    format="[%(asctime)s:%(levelname)s:%(module)s:%(message)s",
+    filemode = 'a'
+    )
+
 
 def main(data, modelName, plotName, eta, epochs):
-    df_OR = pd.DataFrame(data)
-    X, y = prepare_data(df_OR)
+    df = pd.DataFrame(data)
+    logging.info(f"This is the raw dataset: \n{df}")
+    X, y = prepare_data(df)
 
     model = Perceptron(eta=eta, epochs=epochs)
     model.fit(X, y)
@@ -13,7 +27,7 @@ def main(data, modelName, plotName, eta, epochs):
     _ = model.total_loss()
 
     model.save(filename=modelName, model_dir="model")
-    save_plot(df_OR, model, plotName)
+    save_plot(df, model, plotName)
 
 
 if __name__ == "__main__":
@@ -25,5 +39,10 @@ if __name__ == "__main__":
     }
     ETA = 0.3
     EPOCHS = 10
-    main(data=OR, modelName="or.model",plotName="or.png", eta=ETA, epochs=EPOCHS)
-
+    try:
+        logging.info(f">>>>>>>>>>>>>>>>>>>Starting Training for {gate}<<<<<<<<<<<<<<<<<<<<<<<<")
+        main(data=OR, modelName="or.model",plotName="or.png", eta=ETA, epochs=EPOCHS)
+        logging.info(f">>>>>>>>>>>>>>>>>>>Training Complete for {gate}<<<<<<<<<<<<<<<<<<<<<<<<<<\n\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
--- moduler_code/utils/all_utils.py
+++ moduler_code/utils/all_utils.py
@@ -2,8 +2,11 @@ import os
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import numpy as np
+import logging
+
 
 def prepare_data(df, target_col="y"):
+    logging.info("Preparing data for training")
     X = df.drop(target_col, axis=1)
     
     y = df[target_col]
@@ -13,6 +16,7 @@ def prepare_data(df, target_col="y"):
 
 def save_plot(df, model, filename="plot.png", plot_dir="plots"):
     def _create_base_plot(df):
+        logging.info("Creating base plot")
         df.plot(kind="scatter", x="x1", y="x2", c="y", s=100, cmap="coolwarm")
         plt.axhline(y=0, color="black", linestyle="--", linewidth=1)
         plt.axvline(x=0, color="black", linestyle="--", linewidth=1)
@@ -21,6 +25,7 @@ def save_plot(df, model, filename="plot.png", plot_dir="plots"):
         figure.set_size_inches(10, 8)
     
     def _plot_decision_regions(X, y, classifier, resolution=0.02):
+        logging.info("Plotting the decision regions")
         colors = ("cyan", "lightgreen")
         cmap = ListedColormap(colors)
         
@@ -51,3 +56,4 @@ def save_plot(df, model, filename="plot.png", plot_dir="plots"):
     os.makedirs(plot_dir, exist_ok=True)
     plot_path = os.path.join(plot_dir, filename)
     plt.savefig(plot_path)
+    logging.info(f"Plot saved to {plot_path}")
